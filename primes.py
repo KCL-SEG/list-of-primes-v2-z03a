ed2fd046e9c4d9594237b2c49e8b40e9c6e60395
Zahra Amaan <k21011879@kcl.ac.uk>
2022-10-09 18:32:11 +0100

    number entered must be > 1

--- primes.py
+++ primes.py
@@ -2,5 +2,20 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if (number_of_primes < 1):
+        raise ValueError
+        
     list = []
+    i = 2
+    if number_of_primes>0:
+        list.append(i)
+    while len(list) < number_of_primes:
+        prime = True
+        i+=1
+        for j in range(2,i//2+1):
+            if (i%j)==0:
+                prime = False
+                break
+        if prime:
+            list.append(i)
     return list
